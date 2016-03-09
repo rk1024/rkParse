0fd1f8f7806d47683483544ca1f70c84349c2fe9
rookie1024 <rookie1286@gmail.com>
2016-03-08 17:03:51 -0800

    Begin work on lexical system

---
+++ rkParse/Lexical/Lexer.cs
@@ -0,0 +1,11 @@
+﻿using rkParse.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rkParse.Lexical {
+  public class Lexer {
+  }
+}
